6b47d2359e13102c8b64a0fb619bee1ae255c324
anantprsd5 <anantprsd5@gmail.com>
2017-01-26 16:13:43 +0530

    Custom Array Adapter

--- app/src/main/java/com/example/android/miwokapp/Word.java
+++ app/src/main/java/com/example/android/miwokapp/Word.java
@@ -1,33 +1,25 @@
 package com.example.android.miwokapp;
 
-import static android.os.Build.VERSION_CODES.M;
-
 /**
- * Created by saurav on 12/1/17.
+ * Created by anant on 26/1/17.
  */
 
 public class Word {
-
     private String mDefaultTranslation;
-    private String mMiwokkTransaltion;
 
+    private String mMiwokTrasnaltion;
 
-    public Word(String DefaultTranslation, String MiwokkTranslation){
-
-        mDefaultTranslation = DefaultTranslation;
-        mMiwokkTransaltion = MiwokkTranslation;
+    public Word(String defaultTranslation, String miwokTrasnaltion){
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTrasnaltion = miwokTrasnaltion;
 
     }
-
 
     public String getDefaultTranslation(){
-
         return mDefaultTranslation;
-
     }
 
-    public String getMiwokkTransaltion(){
-
-        return mMiwokkTransaltion;
+    public String getMiwokTrasnaltion(){
+        return mMiwokTrasnaltion;
     }
 }
---
+++ app/src/main/java/com/example/android/miwokapp/WordAdapter.java
@@ -0,0 +1,51 @@
+package com.example.android.miwokapp;
+
+import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by anant on 26/1/17.
+ */
+
+public class WordAdapter extends ArrayAdapter<Word> {
+
+    private static final String LOG_TAG = WordAdapter.class.getSimpleName();
+
+    public WordAdapter(Activity context, ArrayList<Word> words){
+        super(context, 0, words);
+    }
+
+
+
+
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View listItemView = convertView;
+        if(listItemView == null){
+            listItemView= LayoutInflater.from(getContext()).inflate(R.layout.list_item,parent,false);
+        }
+
+        Word currentAndroidFlavor = getItem(position);
+
+        // Find the TextView in the list_item.xml layout with the ID version_name
+        TextView nameTextView = (TextView) listItemView.findViewById(R.id.default_text_view);
+        // Get the version name from the current AndroidFlavor object and
+        // set this text on the name TextView
+        nameTextView.setText(currentAndroidFlavor.getDefaultTranslation());
+
+        TextView name1TextView = (TextView) listItemView.findViewById(R.id.miwok_text_view);
+        // Get the version name from the current AndroidFlavor object and
+        // set this text on the name TextView
+        name1TextView.setText(currentAndroidFlavor.getMiwokTrasnaltion());
+        return listItemView;
+    }
+}
